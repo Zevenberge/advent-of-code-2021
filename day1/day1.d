3b4b18aa4a45a220239cdfb1c43bba3a830cbf1b
Marco de Wild <mdwild@sogyo.nl>
2021-12-01 11:28:23 +0100

    Day 1b

--- day1/day1.d
+++ day1/day1.d
@@ -5,11 +5,44 @@ void main()
     enum file = "puzzle.input";
     int previousInput = int.max;
     int amountOfIncreases = 0;
-    foreach(line; File(file, "r").byLine)
+    
+    foreach(group; File(file, "r").byLine
+        .map!(line => line.to!int).array
+        .byThree)
     {
-        int convertedValue = line.to!int;
-        amountOfIncreases += convertedValue > previousInput;
-        previousInput = convertedValue;
+        int summedValue = group.sum;
+        amountOfIncreases += summedValue > previousInput;
+        previousInput = summedValue;
     }
     writeln(amountOfIncreases);
+}
+
+auto byThree(int[] numbers)
+{
+    return ByThree(numbers);
+}
+
+struct ByThree
+{
+    this(int[] numbers)
+    {
+        _numbers = numbers;
+    }
+
+    private int[] _numbers;
+
+    int[] front()
+    {
+        return _numbers[0 .. 3];
+    }
+
+    void popFront()
+    {
+        _numbers = _numbers[1 .. $];
+    }
+
+    bool empty()
+    {
+        return _numbers.length < 3;
+    }
 }
