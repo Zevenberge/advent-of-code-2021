c629ab0c4f0cf150f3cfce223aae6b9e74b4e5c0
Zevenberge <nanayamae@gmail.com>
2021-12-06 07:37:43 +0100

    day 6b

--- day6/day6.d
+++ day6/day6.d
@@ -1,42 +1,50 @@
 import std;
 
-struct LanternFish
+size_t[int] toColony(size_t[] fish)
 {
-    int daysToOffSpring;
-
-    bool hasOffSpring()
+    size_t[int] lanternFish;
+    foreach(i; 0 .. 9)
     {
-        return daysToOffSpring == 0;
+        lanternFish[i] = fish.filter!(f => f == i).count;
     }
-
-    LanternFish advanceDay()
-    {
-        return LanternFish(daysToOffSpring ? daysToOffSpring - 1 : 6);
-    }
-
-    LanternFish resetDay()
-    {
-        return daysToOffSpring ? this : LanternFish(6);
-    }
+    return lanternFish;
 }
 
-LanternFish[] advanceDay(LanternFish[] fish)
+size_t amountOfOffSpring(size_t[int] lanternFish)
 {
-    return fish.map!(f => f.advanceDay).array;
+    return lanternFish[0];
 }
 
-LanternFish[] addOffSpring(LanternFish[] fish)
+size_t[int] advanceDay(size_t[int] lanternFish)
 {
-    return fish.filter!(f => f.hasOffSpring).map!(_ => LanternFish(8)).array;
+    size_t lanternFishToReset = lanternFish[0];
+    foreach(i; 1 .. 9)
+    {
+        lanternFish[i - 1] = lanternFish[i];
+    }
+    lanternFish[8] = 0;
+    lanternFish[6] = lanternFish[6] + lanternFishToReset;
+    return lanternFish;
+}
+
+size_t[int] addOffSpring(size_t[int] lanternFish, size_t amountOfOffSpring)
+{
+    lanternFish[8] = amountOfOffSpring;
+    return lanternFish;
+}
+
+size_t amount(size_t[int] lanternFish)
+{
+    return lanternFish.values.sum;
 }
 
 void main()
 {
-    auto lanternFish = File("puzzle.input", "r").byLine.front.splitter(",").map!(f => LanternFish(f.to!int)).array;
-    foreach(day; 0 .. 80)
+    auto lanternFish = File("puzzle.input", "r").byLine.front.splitter(",").map!(f => f.to!size_t).array.toColony;
+    foreach(day; 0 .. 256)
     {
-        auto offSpring = lanternFish.addOffSpring;
-        lanternFish = lanternFish.advanceDay ~ offSpring;
+        auto offSpring = lanternFish.amountOfOffSpring;
+        lanternFish = lanternFish.advanceDay.addOffSpring(offSpring);
     }
-    lanternFish.length.writeln;
+    lanternFish.amount.writeln;
 }
